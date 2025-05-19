3e23985e6f8b10844bdb2382a4f0c6fe11b38656
Aena Setia <Aena Setia@ICY>
2025-05-19 12:46:23 +1200

    Add project files.

---
+++ Model/Groups.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGroupsApp.Model
+{
+    public class Groups
+    {
+        private int GroupID { get; set; }
+        private string GroupName { get; set; }
+
+        public Groups(int groupID, string groupName)
+        {
+            GroupID = groupID;
+            GroupName = groupName;
+        }//Groups Class
+    }
+}
---
+++ Repositories/StorageManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace SchoolGroupsApp.Repositories
+{
+    public class StorageManager
+    {
+        private SqlConnection conn;
+        // this is the storage manager
+    }
+}
