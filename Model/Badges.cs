a641998f7844300a48533d49a975232e8f3534af
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-06-17 22:10:59 +1200

    I created the badges class.

---
+++ Model/Badges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGroupsApp.Model
+{
+    public class Badges
+    {
+        private int BadgeID {  get; set; }
+        private string BadgeType { get; set; }
+        private string BadgeName { get; set; }
+
+        public Badges(int badgeID, string badgeType, string badgeName)
+        {
+            BadgeID = badgeID;
+            BadgeType = badgeType;
+            BadgeName = badgeName;
+        }
+
+    }
+}
