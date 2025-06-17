a156f4200107435ee46907529380fa5228bfc0e7
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-06-17 21:58:19 +1200

    I created the teachers class.

---
+++ Model/Teachers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGroupsApp.Model
+{
+    public class Teachers
+    {
+        private int TeacherID { get; set; }
+        private string LastName { get; set; }
+        private string FirstName { get; set; }
+
+        public Teachers(int teacherID, string lastName, string firstName)
+        {
+            TeacherID = teacherID;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+    }
+}
