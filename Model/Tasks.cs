df375016eca7e8478c4ec11444fd8a867f957e2b
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-06-17 22:22:56 +1200

    I created the tasks class.

---
+++ Model/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGroupsApp.Model
+{
+    public class Tasks
+    {
+        private int TaskID { get; set; }
+        private string TaskName { get; set; }
+        private int PointsValue { get; set; }
+        private int GroupID { get; set; }
+
+        public Tasks(int taskID, string taskName, int pointsValue, int groupID)
+        {
+            TaskID = taskID;
+            TaskName = taskName;
+            PointsValue = pointsValue;
+            GroupID = groupID;
+        }
+    }
+}
