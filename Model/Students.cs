9024cca95fccb2b57ea0bb14ed15e99dfdd9e347
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-06-17 21:37:18 +1200

    I created the students class.

--- Model/Students.cs
+++ Model/Students.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SchoolGroupsApp.Model
@@ -9,11 +10,18 @@ namespace SchoolGroupsApp.Model
     public class Students
     {
         private int StudentID { get; set; }
-        private string FirstName { get; set; }
         private string LastName { get; set; }
-        private string Homeroom { get; set; }
-        private string Year { get; set; }
+        private string FirstName { get; set; }
+        private int Year { get; set; }
+        private string HomeRoom { get; set; }
 
-
+        public Students(int studentID, string lastName, string firstName, int year, string homeRoom)
+        {
+            StudentID = studentID;
+            LastName = lastName;
+            FirstName = firstName;
+            Year = year;
+            HomeRoom = homeRoom;
+        }
     }
 }
