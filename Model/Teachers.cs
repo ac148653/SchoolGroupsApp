2177643e55a9120a23e44e975a76645e9e1ba77a
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-06-18 12:13:54 +1200

    I added the username and password variables in the teachers class.

--- Model/Teachers.cs
+++ Model/Teachers.cs
@@ -11,12 +11,16 @@ namespace SchoolGroupsApp.Model
         private int TeacherID { get; set; }
         private string LastName { get; set; }
         private string FirstName { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
 
-        public Teachers(int teacherID, string lastName, string firstName)
+        public Teachers(int teacherID, string lastName, string firstName, string userName, string password)
         {
             TeacherID = teacherID;
             LastName = lastName;
             FirstName = firstName;
+            UserName = userName;
+            Password = password;
         }
     }
 }
