2e43ae7f1f5ab47bfe0c826184672db143a56746
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-07-10 15:20:26 +1200

    I could not access the username and password variables of the teachers class in console view and then I realised that the variables were declared as private in the class. Now I have changed all variables in teachers class to public.

--- Model/Teachers.cs
+++ Model/Teachers.cs
@@ -8,11 +8,11 @@ namespace SchoolGroupsApp.Model
 {
     public class Teachers
     {
-        private int TeacherID { get; set; }
-        private string LastName { get; set; }
-        private string FirstName { get; set; }
-        private string UserName { get; set; }
-        private string Password { get; set; }
+        public int TeacherID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
 
         public Teachers(int teacherID, string lastName, string firstName, string userName, string password)
         {
