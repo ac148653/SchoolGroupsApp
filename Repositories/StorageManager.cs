bc792c562bc7253bb9c75a82c9888eb3f5befa1a
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-07-11 14:07:12 +1200

    I changed the groupId method in the storage manager but the error is still there.

--- Repositories/StorageManager.cs
+++ Repositories/StorageManager.cs
@@ -273,7 +273,6 @@ namespace SchoolGroupsApp.Repositories
         {
             using SqlCommand cmd = new SqlCommand("SELECT groupID FROM GroupManagement.groups WHERE groupName = @GroupName", conn);
             {
-                cmd.Parameters.AddWithValue("@GroupName", groupName);
                 cmd.Parameters.AddWithValue("@GroupID", groupID);
                 return Convert.ToInt32(cmd.ExecuteScalar());
             }
