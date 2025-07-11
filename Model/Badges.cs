7e151aea36e2a11df93289beff5fb5f78e00a730
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-07-11 14:55:28 +1200

    I realised that I had declared variables in badges as private so I changed them to public.

--- Model/Badges.cs
+++ Model/Badges.cs
@@ -8,9 +8,9 @@ namespace SchoolGroupsApp.Model
 {
     public class Badges
     {
-        private int BadgeID {  get; set; }
-        private string BadgeLevel { get; set; }
-        private string BadgeName { get; set; }
+        public int BadgeID {  get; set; }
+        public string BadgeLevel { get; set; }
+        public string BadgeName { get; set; }
 
         public Badges(int badgeID, string badgeLevel, string badgeName)
         {
