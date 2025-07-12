c960280ec931eaf57c2db8a62c0ee6e555623a06
ac148653@avcol.school.nz <ac148653@avcol.school.nz>
2025-07-12 13:09:56 +1200

    I updated the student menu switch case.

--- Program.cs
+++ Program.cs
@@ -684,7 +684,8 @@ namespace SchoolGroupsApp
                         JoinGroup();
                         break;
                     case 2:
-          
+                        UnenrollGroup();
+                        break;
                     case 3:
                     
                     case 4:
